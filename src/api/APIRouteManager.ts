17a9a3cc74d8bc77cf3426c13f2a6bd17f2e2f82
Ethan Campbell <74080985+goatm0n@users.noreply.github.com>
2024-02-01 23:08:04 +0000

    Update APIRouteManager.ts
    use new backened url

--- src/api/APIRouteManager.ts
+++ src/api/APIRouteManager.ts
@@ -2,7 +2,7 @@
 
 const baseUrl: string= window.location.protocol + "//" + window.location.host;
 
-const baseApiUrl: string = "http://127.0.0.1:8000"
+const baseApiUrl: string = "http://ec2-34-245-85-205.eu-west-1.compute.amazonaws.com"
 
 export const ACCOUNTS_API_URL: string = baseApiUrl + "/accounts/api";
 export const PROFILES_API_URL: string = baseApiUrl + "/profiles/api";
