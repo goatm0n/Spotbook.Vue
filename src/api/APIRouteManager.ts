889e3329f695cc20dcfc59dbef04e4a1474d5deb
Ethan Campbell <74080985+goatm0n@users.noreply.github.com>
2024-02-02 20:58:45 +0000

    Update APIRouteManager.ts

--- src/api/APIRouteManager.ts
+++ src/api/APIRouteManager.ts
@@ -2,7 +2,7 @@
 
 const baseUrl: string= window.location.protocol + "//" + window.location.host;
 
-const baseApiUrl: string = "http://ec2-34-245-85-205.eu-west-1.compute.amazonaws.com"
+const baseApiUrl: string = "https://ec2-34-245-85-205.eu-west-1.compute.amazonaws.com"
 
 export const ACCOUNTS_API_URL: string = baseApiUrl + "/accounts/api";
 export const PROFILES_API_URL: string = baseApiUrl + "/profiles/api";
