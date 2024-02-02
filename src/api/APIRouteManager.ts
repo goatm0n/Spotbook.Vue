b81b1f0865799d70f67c9d1d95a7126ff47442b9
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2024-02-02 23:06:13 +0000

    point to domain api

--- src/api/APIRouteManager.ts
+++ src/api/APIRouteManager.ts
@@ -2,7 +2,7 @@
 
 const baseUrl: string= window.location.protocol + "//" + window.location.host;
 
-const baseApiUrl: string = "http://127.0.0.1:8000"
+const baseApiUrl: string = "http://spotbookserver.spotbookapp.com"
 
 export const ACCOUNTS_API_URL: string = baseApiUrl + "/accounts/api";
 export const PROFILES_API_URL: string = baseApiUrl + "/profiles/api";
